42b043eb27e2882fdc1fea1baa8b5e31b7ded4d8
kenhsu <kenhsu@fashionguide.com.tw>
2015-10-22 15:24:47 +0800

    test

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = '';
-$db['default']['password'] = '';
+$db['default']['password'] = 'a128979296';
 $db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
