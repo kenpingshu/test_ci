438935b7114a1660a853875a8b688eafe1ea7670
kenhsu <kenhsu@fashionguide.com.tw>
2015-10-23 10:05:39 +0800

    Merge dev
    Conflicts:
    	application/config/database.php

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = '';
-$db['default']['password'] = '666666';
+$db['default']['password'] = '77777';
 $db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
